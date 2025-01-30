0aee12554867cf4e37920cd2e02c045b82218f91
Mohammad <mohammadreza.roosta71@gmail.com>
2025-01-30 19:16:31 +0330

    Apply `font2-0.8.5` patch

--- config.h
+++ config.h
@@ -6,6 +6,11 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+	"Hack Nerd Font:pixelsize=17.75:antialias=true:autohint=true",
+};
+
 static int borderpx = 2;
 
 /*
